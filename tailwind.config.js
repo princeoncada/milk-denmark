d4839aa88d52427967173568ae056072a50af928
prince <prince.oncada@gmail.com>
2024-07-09 20:27:00 -0400

    fixed hero and finished content one

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./layout/*.liquid",
-    "./templates/*.liquid",
-    "./templates/customers/*.liquid",
-    "./sections/*.liquid",
-    "./snippets/*.liquid",
-
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "hero-image": "url(//breastmil.myshopify.com/cdn/shop/t/3/assets/baby_large.jpg?v=90550805258474356221720114194)",
-        "passion-image": "url(//breastmil.myshopify.com/cdn/shop/t/3/assets/berryleaf_large.png?v=142001938210005096811720222146)"
-      },
-      fontFamily: {
-        default: ["'Nanum Myeongjo', serif"]
-      }
-    },
-  },
-  plugins: [],
+	content: [
+		"./layout/*.liquid",
+		"./templates/*.liquid",
+		"./templates/customers/*.liquid",
+		"./sections/*.liquid",
+		"./snippets/*.liquid",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				default: ["'Nanum Myeongjo', serif"],
+			},
+		},
+	},
+	plugins: [],
 };
